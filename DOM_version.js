405a4ca19e462f4fb7b80a573ce022f09c6bb67d
Chenerywoman <rachelmchenery@hotmail.com>
2020-11-19 02:10:58 +0000

    finalising DOM logic & CSS and some responsiveness

---
+++ DOM_version.js
@@ -0,0 +1,590 @@
+const witchesWizards = [
+
+    {
+        name: "Gilderoy Lockhart",
+        courage: 22,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22,
+        image: "https://static.wikia.nocookie.net/harrypotter/images/d/d4/Gilderoy_Lockhart_COS_promo.jpg/revision/latest/scale-to-width-down/902?cb=20160905192348",
+        alt: "Gilderoy Lockhart",
+
+    },
+    {
+        name: "Albus Potter",
+        courage: 29,
+        potions: 11, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22,
+        img: "",
+        alt: "Albus Potter"
+
+    },
+    {
+        name: "Rubeus Hagrid",
+        courage: 28,
+        potions: 15, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+
+    },
+    {
+        name: "Cho Chang",
+        courage: 27,
+        potions: 18, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+
+    },
+    {
+        name: "Luna Lovegood",
+        courage: 26,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+
+    },
+    {
+        name: "Ron Weasley",
+        courage: 25,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+    },
+    {
+        name: "Neville Longbottom",
+        courage: 48,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+    },
+    {
+        name: "Fred Weasley",
+        courage: 29,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+
+    },
+    {
+        name: "George Weasley",
+        courage: 29,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+    },
+    {
+        name: "Newt Schamander",
+        courage: 21,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22,
+        image: "",
+        alt: "Newt Schamander"
+
+    },
+    {
+        name: "Draco Malfoy",
+        courage: 15,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+    },
+    {
+        name: "Ginny Weasley",
+        courage: 19,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+    },
+    {
+        name: "Padma Patil",
+        courage: 18,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+
+    },
+    {
+        name: "Cedric Diggory",
+        courage: 32,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+
+    },
+    {
+        name: "James Potter",
+        courage: 25,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+
+    },
+    {
+        name: "Bill Weasley",
+        courage: 25,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22,
+        img: "https://www.google.com/search?q=pictures+of++bill+weasley&rlz=1C5CHFA_enGB769GB770&sxsrf=ALeKk03esHjwxvDqph-1Z90Rs70RY2VA4A:1605550197054&tbm=isch&source=iu&ictx=1&fir=sxcOI91Yig7rYM%252CHYg41lCtnZ_pbM%252C_&vet=1&usg=AI4_-kTkmThDfrpULDkcLGnma2L__NMPbg&sa=X&ved=2ahUKEwi9-afJ1IftAhWHYsAKHbCNCYAQ9QF6BAgMEEI#imgrc=sxcOI91Yig7rYM",
+        alt: "Bill Weasley"
+    },
+    {
+        name: "Filius Flitwick",
+        courage: 25,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+
+    },
+    {
+        name: "Lucius Malfoy",
+        courage: 25,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+    },
+    {
+        name: "Molly Weasley",
+        courage: 25,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22,
+        img: "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters_opt/p-harry-potter-julie-walters.jpg",
+        alt:"Molly Weasley"
+    },
+    {
+        name: "Sirius Black",
+        courage: 25,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+
+    },
+    {
+        name: "Lily Potter",
+        courage: 25,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+
+    },
+    {
+        name: "Harry Potter",
+        courage: 25,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+
+    },
+    {
+        name: "Horace Slughorn",
+        courage: 25,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+
+    },
+    {
+        name: "Hermione Granger",
+        courage: 25,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+
+    },
+    {
+        name: "Remus Lupin",
+        courage: 25,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+
+    },
+    {
+        name: "Bellatrix Lestrange",
+        courage: 25,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+
+    },
+    {
+        name: "Minerva McGonagall",
+        courage: 25,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+
+    },
+    {
+        name: "Severus Snape",
+        courage: 25,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+
+    },
+    {
+        name: "Tom Riddle",
+        courage: 25,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22,
+        img: "https://www.google.com/search?q=pictures+of+tom+riddle&rlz=1C5CHFA_enGB769GB770&sxsrf=ALeKk01kbbNIJBhBgR7CNJ4a8SFOXOh00Q:1605550096961&tbm=isch&source=iu&ictx=1&fir=W-TJlj6tM82eEM%252C00TVcE3Avzr1zM%252C_&vet=1&usg=AI4_-kQpX-b6sU6-nXjGK5Q73qNfOnn1Og&sa=X&ved=2ahUKEwiE4MqZ1IftAhUIHcAKHcrNBLsQ9QF6BAgFEDU&biw=1920&bih=1001#imgrc=W-TJlj6tM82eEM",
+        alt: "Tom Riddle"
+    },
+    {
+        name: "Albus Dumbledore",
+        courage: 25,
+        potions: 10, 
+        spells: 29,
+        broomSkill: 15,
+        wandPower: 22
+
+    }
+];
+
+let shuffleButton = document.getElementsByClassName("shuffle")[0];
+let selectCategoriesMenu = document.getElementsByTagName("select")[0];
+let compareButton = document.getElementsByClassName("compare")[0];
+let moveButton = document.getElementsByClassName("move")[0];
+
+let playerOneTopCard = document.getElementsByClassName("player-one")[0].getElementsByClassName("card")[0];
+let playerTwoTopCard = document.getElementsByClassName("player-two")[0].getElementsByClassName("card")[0];
+
+const hideElements = (element) => {
+    element.style.display = "none";
+}
+
+const showElements = (element) => {
+    element.style.display = "inline-block";
+}
+
+/* need individual images */
+const tempImage = "https://imageproxy.themaven.net//https%3A%2F%2Fwww.biography.com%2F.image%2FMTQ3OTg2NjQ0NDg0NDk4OTEx%2Fhp_cropped_gettyimages-113941726.jpg"
+
+const populateCards = (element, array) => {
+    // element.getElementsByTagName("img")[0].src = array[0].image;
+    element.getElementsByTagName("img")[0].src = tempImage;
+    element.getElementsByTagName("img")[0].alt = array[0].alt;
+    element.getElementsByClassName("witch-wizard")[0].getElementsByTagName("span")[0].innerText = array[0].name;
+    element.getElementsByClassName("courage")[0].innerText = array[0].courage;
+    element.getElementsByClassName("potions")[0].innerText = array[0].potions;
+    element.getElementsByClassName("spells")[0].innerText = array[0].spells;
+    element.getElementsByClassName("broom")[0].innerText = array[0].broomSkill;
+    element.getElementsByClassName("wand")[0].innerText = array[0].wandPower;
+}
+
+const showPlayer = (player, element) => {
+    player == 1 ? element.innerText = "One" : element.innerText = "Two";
+}
+
+const limbo = [];
+const playerOne = [];
+const playerTwo = [];
+let playerTurn = 1;
+let category = "courage";
+let roundWinner = 0;
+
+populateCards(document.getElementsByClassName("card-pile")[0].getElementsByClassName("card")[0], witchesWizards);
+document.getElementsByClassName("card-pile")[0].getElementsByClassName("number-of-cards")[0].innerHTML = witchesWizards.length;
+
+shuffleButton.addEventListener("click", () => {
+
+    const cardsToShuffle = [...witchesWizards];
+    playerOne.splice(0, playerOne.length);
+    playerTwo.splice(0, playerTwo.length);
+
+    for (i = 0; i < witchesWizards.length; i++) {
+        let randomInt = Math.floor(Math.random() * cardsToShuffle.length)
+        playerTurn == 1 ? playerOne.push(cardsToShuffle[randomInt]) : playerTwo.push(cardsToShuffle[randomInt]);
+        cardsToShuffle.splice(randomInt, 1);
+        playerTurn == 1 ? playerTurn = 2 : playerTurn = 1;
+    }
+
+    document.getElementsByClassName("player-one")[0].style.width = "50%";
+    populateCards(playerOneTopCard, playerOne)
+    document.getElementsByClassName("player-one")[0].getElementsByClassName("number-of-cards")[0].innerHTML = playerOne.length;
+
+    document.getElementsByClassName("player-two")[0].style.width = "50%";
+    populateCards(playerTwoTopCard, playerTwo)
+    document.getElementsByClassName("player-two")[0].getElementsByClassName("number-of-cards")[0].innerHTML = playerTwo.length;
+
+    hideElements(document.getElementsByClassName("card-pile")[0]);
+    hideElements(shuffleButton);
+    hideElements(document.getElementsByClassName("game")[0]);
+    hideElements(document.getElementsByClassName("game")[1]);
+
+    showPlayer(playerTurn, document.getElementsByClassName("turn")[0].getElementsByTagName("span")[0])
+
+    showElements(document.getElementsByClassName("turn")[0]);
+    showElements(document.getElementsByClassName("player-one")[0]);
+    showElements(document.getElementsByClassName("player-two")[0]);
+    showElements(document.getElementsByTagName("label")[0]);
+    showElements(selectCategoriesMenu);
+    showElements(compareButton);
+
+    let div = document.createElement("div");
+    let node = document.createTextNode("Hogwarts Top Trumps");
+    div.appendChild(node);
+
+    if (playerTurn == 1) {
+
+        playerTwoTopCard.getElementsByTagName("img")[0].style.display = "none";
+        playerTwoTopCard.getElementsByTagName("h3")[0].style.display = "none";
+        playerTwoTopCard.getElementsByTagName("table")[0].style.display = "none";
+
+        playerTwoTopCard.appendChild(div);
+        playerTwoTopCard.getElementsByTagName("div")[0].style.fontSize = "50px";
+        playerTwoTopCard.getElementsByTagName("div")[0].setAttribute("id", "player_two_back");
+        playerTwoTopCard.getElementsByTagName("div")[0].style.transform = "rotate(45deg)"
+        playerTwoTopCard.getElementsByTagName("div")[0].style.color = "silver";
+        playerTwoTopCard.getElementsByTagName("div")[0].style.zIndex = "1";
+        playerTwoTopCard.getElementsByTagName("div")[0].style.padding = "95px 50px 95px 50px";
+        playerTwoTopCard.style.backgroundColor = "#00165e";
+        
+
+    } else {
+        playerOneTopCard.getElementsByTagName("img")[0].style.display = "none";
+        playerOneTopCard.getElementsByTagName("h3")[0].style.display = "none";
+        playerOneTopCard.getElementsByTagName("table")[0].style.display = "none";
+
+        playerOneTopCard.appendChild(div);
+        playerOneTopCard.getElementsByTagName("div")[0].style.fontSize = "50px";
+        playerOneTopCard.getElementsByTagName("div")[0].setAttribute("id", "player_one_back");
+        playerOneTopCard.getElementsByTagName("div")[0].style.transform = "rotate(45deg)";
+        playerOneTopCard.getElementsByTagName("div")[0].style.color = "silver";
+        playerOneTopCard.getElementsByTagName("div")[0].style.zIndex = "1";
+        playerOneTopCard.getElementsByTagName("div")[0].style.padding = "95px 50px 95px 50px";
+        playerOneTopCard.style.backgroundColor = "#00165e";
+    }
+
+})
+
+selectCategoriesMenu.addEventListener("change", (event) => {
+
+    category = event.target.value
+    console.log(category)
+
+})
+
+compareButton.addEventListener("click", () => {
+
+    if (playerTurn == 1) {
+
+        playerTwoTopCard.getElementsByTagName("img")[0].style.display = "block";
+        playerTwoTopCard.getElementsByTagName("img")[0].style.margin = "auto";
+        playerTwoTopCard.getElementsByTagName("h3")[0].style.display = "inline-block";
+        playerTwoTopCard.getElementsByTagName("table")[0].style.display = "inline-block";
+
+        playerTwoTopCard.style.color = "#00165e";
+        playerTwoTopCard.style.backgroundColor = "#9c1203";
+    
+        playerTwoTopCard.getElementsByTagName("div")[0].remove();
+
+    } else {
+
+        playerOneTopCard.getElementsByTagName("img")[0].style.display = "block";
+        playerOneTopCard.getElementsByTagName("img")[0].style.margin = "auto";
+        playerOneTopCard.getElementsByTagName("h3")[0].style.display = "inline-block";
+        playerOneTopCard.getElementsByTagName("table")[0].style.display = "inline-block";
+
+        playerOneTopCard.style.color = "#00165e";
+        playerOneTopCard.style.backgroundColor = "#9c1203";
+
+        playerOneTopCard.getElementsByTagName("div")[0].remove();
+    }
+
+        hideElements(document.getElementsByTagName("label")[0]);
+        hideElements(selectCategoriesMenu);
+        hideElements(document.getElementsByClassName("turn")[0]);
+        hideElements(compareButton);
+        showElements(moveButton);
+
+        if (playerOne[0][category] > playerTwo[0][category]) {
+            roundWinner = 1; 
+            showElements(document.getElementsByClassName("round winner")[0])
+            
+        } else if (playerTwo[0][category] > playerOne[0][category]) {
+            roundWinner = 2;
+
+            showElements(document.getElementsByClassName("round winner")[0])
+
+        } else {
+            roundWinner = 0;
+            showElements(document.getElementsByClassName("round limbo")[0])
+        }
+
+        showPlayer(roundWinner, document.getElementsByClassName("round winner")[0].getElementsByTagName("span")[0] )
+
+    });
+
+    moveButton.addEventListener("click", () => {
+
+        if (roundWinner == 1) {
+
+            playerOne.push(playerTwo[0]);
+            playerTwo.shift();
+            let frontCard = playerOne.shift();
+            playerOne.push(frontCard);
+
+            if (limbo.length > 0) {
+    
+                for (i = 0; i < limbo.length; i++){
+                    playerOne.push(limbo[i])
+            }
+
+            limbo.splice(0, limbo.length);
+
+            }
+
+            playerTurn = 1;
+
+        } else if (roundWinner == 2) {
+
+            playerTwo.push(playerOne[0]);
+            playerOne.shift();
+            let frontCard = playerTwo.shift();
+            playerTwo.push(frontCard);
+
+            if (limbo.length > 0) {
+        
+                for (i = 0; i < limbo.length; i++){
+                    playerTwo.push(limbo[i]);
+                }
+
+                limbo.splice(0, limbo.length);
+
+            }
+
+            playerTurn = 2;
+
+        } else {
+
+            limbo.push(playerOne[0], playerTwo[0])
+            playerOne.shift();
+            playerTwo.shift();
+
+        }
+
+        hideElements(moveButton);
+        hideElements(document.getElementsByClassName("round winner")[0]);
+        hideElements(document.getElementsByClassName("round limbo")[0]);
+
+        if (playerOne.length == witchesWizards.length || playerTwo.length == witchesWizards.length){
+
+            showPlayer(roundWinner, document.getElementsByClassName("game winner")[0].getElementsByTagName("span")[0] )
+            showElements(document.getElementsByClassName("game winner")[0]);
+            showElements(document.getElementsByClassName("new game")[0]);
+            showElements(shuffleButton);
+
+            if (playerOne.length == witchesWizards.length){
+
+                hideElements(document.getElementsByClassName("player-two")[0])
+                document.getElementsByClassName("player-one")[0].style.width = "100%"
+                populateCards(playerOneTopCard, playerOne)
+                document.getElementsByClassName("player-one")[0].getElementsByClassName("number-of-cards")[0].innerHTML = playerOne.length;
+            
+            } else {
+
+                hideElements(document.getElementsByClassName("player-one")[0])
+                document.getElementsByClassName("player-two")[0].style.width = "100%"
+                populateCards(playerTwoTopCard, playerTwo)
+                document.getElementsByClassName("player-two")[0].getElementsByClassName("number-of-cards")[0].innerHTML = playerTwo.length;
+            }
+
+        } else {
+
+            let div = document.createElement("div");
+            let node = document.createTextNode("Hogwarts Top Trumps");
+            div.appendChild(node);
+
+            if (playerTurn == 1) {
+
+                playerTwoTopCard.getElementsByTagName("img")[0].style.display = "none";
+                playerTwoTopCard.getElementsByTagName("h3")[0].style.display = "none";
+                playerTwoTopCard.getElementsByTagName("table")[0].style.display = "none";
+
+                playerTwoTopCard.appendChild(div);
+                playerTwoTopCard.getElementsByTagName("div")[0].setAttribute("id", "player_two_back");
+                playerTwoTopCard.getElementsByTagName("div")[0].style.fontSize = "50px"
+                playerTwoTopCard.getElementsByTagName("div")[0].style.transform = "rotate(45deg)"
+                playerTwoTopCard.getElementsByTagName("div")[0].style.color = "silver";
+                playerTwoTopCard.getElementsByTagName("div")[0].style.zIndex = "1";
+                playerTwoTopCard.getElementsByTagName("div")[0].style.padding = "95px 50px 95px 50px";
+                playerTwoTopCard.style.backgroundColor = "#00165e";
+
+            } else {
+                playerOneTopCard.getElementsByTagName("img")[0].style.display = "none";
+                playerOneTopCard.getElementsByTagName("h3")[0].style.display = "none";
+                playerOneTopCard.getElementsByTagName("table")[0].style.display = "none";
+
+                playerOneTopCard.appendChild(div);
+                playerOneTopCard.getElementsByTagName("div")[0].setAttribute("id", "player_one_back");
+                playerOneTopCard.getElementsByTagName("div")[0].style.fontSize = "50px"
+                playerOneTopCard.getElementsByTagName("div")[0].style.transform = "rotate(45deg)"
+                playerOneTopCard.getElementsByTagName("div")[0].style.color = "silver";
+                playerOneTopCard.getElementsByTagName("div")[0].style.zIndex = "1";
+                playerOneTopCard.getElementsByTagName("div")[0].style.padding = "95px 50px 95px 50px";
+                playerOneTopCard.style.backgroundColor = "#00165e";
+            }
+
+            populateCards(playerOneTopCard, playerOne)
+            document.getElementsByClassName("player-one")[0].getElementsByClassName("number-of-cards")[0].innerHTML = playerOne.length;
+            
+            populateCards(playerTwoTopCard, playerTwo)
+            document.getElementsByClassName("player-two")[0].getElementsByClassName("number-of-cards")[0].innerHTML = playerTwo.length;
+
+            showPlayer(playerTurn, document.getElementsByClassName("turn")[0].getElementsByTagName("span")[0]);
+            showElements(document.getElementsByClassName("turn")[0]);
+            showElements(document.getElementsByTagName("label")[0]);
+            showElements(selectCategoriesMenu);
+            showElements(compareButton);
+
+        }
+  
+    })
+    
+    
+    
+    
+
+
+ 
+
+
+
+
+
+
