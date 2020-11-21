1b7e074e4c264b6825aa2780545277fbe366464f
Chenerywoman <rachelmchenery@hotmail.com>
2020-11-21 18:12:12 +0000

    showing round winner in red, adding current category to the display & resizing

--- DOM_version.js
+++ DOM_version.js
@@ -36,33 +36,33 @@ class Graduate {
 }
 
 const albusDumbledore = new Graduate("Albus Dumbledore", 1892, 15, 4, 7, 27, "https://cutewallpaper.org/21/dumbledore-images/Jude-Law-to-Play-Young-Dumbledore-in-Fantastic-Beasts-Sequel.jpg");
-const harryPotter = new Graduate("Harry Potter", 1991, 11, 7, 4, 3, "https://imageproxy.themaven.net//https%3A%2F%2Fwww.biography.com%2F.image%2FMTQ3OTg2NjQ0NDg0NDk4OTEx%2Fhp_cropped_gettyimages-113941726.jpg" );
-const severusSnape = new Graduate("Severus Snape", 1971, 13, 2, 9, 26, "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters_opt/p-harry-potter-alan-rickman.jpg");
+const harryPotter = new Graduate("Harry Potter", 1991, 11, 7, 4, 28, "https://imageproxy.themaven.net//https%3A%2F%2Fwww.biography.com%2F.image%2FMTQ3OTg2NjQ0NDg0NDk4OTEx%2Fhp_cropped_gettyimages-113941726.jpg" );
+const severusSnape = new Graduate("Severus Snape", 1971, 13, 10, 9, 26, "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters_opt/p-harry-potter-alan-rickman.jpg");
 const siriusBlack = new Graduate("Sirius Black", 1971, 12, 6, 5, 22, "https://static.wikia.nocookie.net/harrypotter/images/b/bc/OOTP_promo_front_closeup_Sirius_Black.jpg/revision/latest?cb=20150918055024");
 const ronWeasley = new Graduate("Ron Weasley", 1991, 14, 11, 4, 29, "https://1.bp.blogspot.com/-_dWDlmiEwSE/Wr-CeiguKBI/AAAAAAAAFNk/ZnXzV6LL0e0PKfjQQG5qsgJKhJoNreZ-ACLcBGAs/s1600/Ron%2BWeasley%2B%2528I%2529.jpg");
 const hermioneGranger = new Graduate("Hermione Granger", 1991, 10, 5, 3, 29, "https://static.wikia.nocookie.net/characters/images/a/a5/Latest_%2810%29.jpg/revision/latest?cb=20141230074301");
 const tomRiddle = new Graduate("Tom Riddle", 1938, 13, 11, 10, 25, "https://www.google.com/search?q=pictures+of+tom+riddle&rlz=1C5CHFA_enGB769GB770&sxsrf=ALeKk01kbbNIJBhBgR7CNJ4a8SFOXOh00Q:1605550096961&tbm=isch&source=iu&ictx=1&fir=W-TJlj6tM82eEM%252C00TVcE3Avzr1zM%252C_&vet=1&usg=AI4_-kQpX-b6sU6-nXjGK5Q73qNfOnn1Og&sa=X&ved=2ahUKEwiE4MqZ1IftAhUIHcAKHcrNBLsQ9QF6BAgFEDU&biw=1920&bih=1001#imgrc=W-TJlj6tM82eEM");
 const cedricDiggory = new Graduate("Cedric Diggory", 1989, 12, 2, 4, 21, "https://static.wikia.nocookie.net/harrypotter/images/9/90/Cedric_Diggory_Profile.png/revision/latest?cb=20161123045136");
-const bellatrixLeStrange = new Graduate("Bellatrix LeStrange", 1962, 12, 7, 8, 23, "https://media.beliefnet.com/~/media/photos-with-attribution/entertainment/celebrities/bellatrix_lestrange_helena_bonham_carter/bellatrix-lestrange-10_credit-warner-bros.jpg?as=1&extension=webp");
+const bellatrixLeStrange = new Graduate("Bellatrix LeStrange", 1962, 12, 7, 8, 13, "https://media.beliefnet.com/~/media/photos-with-attribution/entertainment/celebrities/bellatrix_lestrange_helena_bonham_carter/bellatrix-lestrange-10_credit-warner-bros.jpg?as=1&extension=webp");
 const rubeusHagrid = new Graduate("Rubeus Hagrid", 1940, 16, 1, 3, 24, "https://static.wikia.nocookie.net/harrypotter/images/f/fe/Rubeus_Hagrid.png/revision/latest?cb=20161123044204");
 const fleurDelacour = new Graduate("Fleur Delacour", 1994, 9, 7, 2, 12, "https://images.ctfassets.net/usf1vwtuqyxm/1FGP9adjbqe4Q8AgGaIQIC/1de41e9d4dd4eb2de0a63fe369989fe5/FleurDelacour_WB_F4_FleurDelacourCloseup_Still_080615_Land.jpg?w=914");
 const ginnyWeasley = new Graduate("Ginny Weasley", 1992, 7, 12, 2, 20, "https://bloximages.newyork1.vip.townnews.com/roanoke.com/content/tncms/assets/v3/editorial/b/40/b402a776-942a-11e3-b3c3-0017a43b2370/52fbe4691cc2e.preview-300.jpg?crop=300%2C300%2C0%2C50&resize=1200%2C1200&order=crop%2Cresize");
-const argusFinch = new Graduate("Argus Filch", 1973, 6, 5, 1, 8,"https://static.wikia.nocookie.net/harrypotter/images/c/c8/Mainfulcd.jpg/revision/latest?cb=20200413210532");
+const argusFinch = new Graduate("Argus Filch", 1973, 1, 1, 1, 1,"https://static.wikia.nocookie.net/harrypotter/images/c/c8/Mainfulcd.jpg/revision/latest?cb=20200413210532");
 const remusLupin = new Graduate("Remus Lupin", 1971, 0, 4, 8, 19, "https://static.wikia.nocookie.net/harrypotterfanon/images/0/0c/Remus_lupin.jpg/revision/latest/top-crop/width/360/height/450?cb=20170802175350");
 const lunaLovegood = new Graduate("Luna Lovegood", 1992, 6, 2, 1, 11, "https://images.ctfassets.net/usf1vwtuqyxm/t6GVMDanqSKGOKaCWi8oi/74b6816d9f913623419b98048ec87d25/LunaLovegood_WB_F5_LunaLovegoodPromoCloseUp_Promo_080615_Port.jpg");
 const dracoMalfoy = new Graduate("Draco Malfoy", 1991, 10, 4, 4, 18, "https://imgix.bustle.com/rehost/2016/9/13/6a08c5f4-def1-4c78-9227-2dbd4c268b6a.jpg");
-const mollyWeasley = new Graduate("Molly Weasley", 1961, 24, 1, 17, "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters_opt/p-harry-potter-julie-walters.jpg");
+const mollyWeasley = new Graduate("Molly Weasley", 1961, 24, 8, 8, 17, "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters_opt/p-harry-potter-julie-walters.jpg");
 const nevilleLongbottom = new Graduate("Neville Longbottom", 1991, 13, 4, 1, 25, "https://static.wikia.nocookie.net/harrypotter/images/b/b6/Neville_Longbottom.jpg/revision/latest/scale-to-width-down/340?cb=20140610141612");
 const minervaMcGonagall = new Graduate("Minerva McConagall", 1947, 9, 8, 6, 16, "https://static.wikia.nocookie.net/harrypotter/images/f/f9/PromoHP1_Minerva_McGonagall_2.jpg/revision/latest/scale-to-width-down/340?cb=20150810173547");
 const sybillTrelawney = new Graduate("Sybill Trelawney", 1973, 7, 4, 1, 3, "https://i.pinimg.com/originals/91/eb/1f/91eb1f483557eaf49a99848bde8f5d06.jpg");
 const nymphadoraTonks = new Graduate("Nymphadora Tonks", 1984, 14, 4, 2, 4, "https://cdn.staticneo.com/w/harrypotter/thumb/Nymphadora_Tonks.jpg/200px-Nymphadora_Tonks.jpg");
 const horaceSlughorn = new Graduate("Horace Slughorn", 1931, 10, 8, 2, 9, "https://www.moviemaniauk.co.uk/assets/professor-horace-slughorn.jpg");
 const victorKrum = new Graduate("Victor Krum", 1994, 10, 2, 5, 7, "https://static.wikia.nocookie.net/harrypotter/images/9/93/Viktor_Krum_promo_Yule_Ball.png/revision/latest/top-crop/width/360/height/450?cb=20120207173113");
-const gilderoyLockhart = new Graduate("Gilderoy Lockhart", 1992, 9, 11, 2, 6,  "https://static.wikia.nocookie.net/harrypotter/images/1/1a/Kenneth_Branagh_as_Gilderoy-Lockhart_%286%29.jpg/revision/latest?cb=20181009213748");
+const gilderoyLockhart = new Graduate("Gilderoy Lockhart", 1972, 9, 11, 2, 6,  "https://static.wikia.nocookie.net/harrypotter/images/1/1a/Kenneth_Branagh_as_Gilderoy-Lockhart_%286%29.jpg/revision/latest?cb=20181009213748");
 const arthurWeasley = new Graduate("Arthur Weasley", 1961, 12, 22, 2, 15, "https://static.wikia.nocookie.net/p__/images/2/25/Arthur-weasley-1-.jpg/revision/latest?cb=20170123000138&path-prefix=protagonist");
 const luciusMalfoy = new Graduate("Lucius Malfoy", 1965, 18, 4, 7, 14, "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters_opt/p-harry-potter-jason-isaacs.jpg");
 const choChang = new Graduate("Cho Chang", 1990, 6, 2, 2, 5, "https://static.wikia.nocookie.net/harrypotter/images/1/1e/Cho_Chang.jpg/revision/latest?cb=20180322164130");
-const peterPettigrew = new Graduate("Peter Pettigrew", 1971, 1, 9, 6, 13, "https://i.pinimg.com/originals/6f/0e/3a/6f0e3a79374ce894ccfcfc5c81c6675e.jpg");
+const peterPettigrew = new Graduate("Peter Pettigrew", 1971, 1, 9, 6, 9, "https://i.pinimg.com/originals/6f/0e/3a/6f0e3a79374ce894ccfcfc5c81c6675e.jpg");
 const fredWeasley = new Graduate("Fred Weasley", 1985, 10, 12, 8, 12, "https://cdn.staticneo.com/w/harrypotter/thumb/Fred.PNG/200px-Fred.PNG" );
 const georgeWeasley = new Graduate("George Weasley", 1985, 10, 13, 7, 12, "https://static.wikia.nocookie.net/harrypotterfanon/images/b/be/George_Weasley_II.jpg/revision/latest/top-crop/width/360/height/450?cb=20160417155929");
 
@@ -104,7 +104,7 @@ const limbo = [];
 const playerOne = [];
 const playerTwo = [];
 let playerTurn = 1;
-let category = "courage";
+let category = "matriculation";
 let roundWinner = 0;
 
 populateCards(document.getElementsByClassName("card-pile")[0].getElementsByClassName("card")[0], graduates);
@@ -134,11 +134,15 @@ shuffleButton.addEventListener("click", () => {
     hideElements(document.getElementsByClassName("game")[0]);
     hideElements(document.getElementsByClassName("game")[1]);
 
-    showPlayer(playerTurn, document.getElementsByClassName("turn")[0].getElementsByTagName("span")[0])
-
-    showElements(document.getElementsByClassName("turn")[0]);
     showElements(document.getElementsByClassName("player-one")[0]);
     showElements(document.getElementsByClassName("player-two")[0]);
+
+    document.getElementsByClassName("set-aside")[0].style.display = "block";
+    document.getElementsByClassName("set-aside")[0].getElementsByTagName("span")[0].innerHTML = limbo.length;
+
+    showPlayer(playerTurn, document.getElementsByClassName("turn")[0].getElementsByTagName("span")[0])
+    showElements(document.getElementsByClassName("turn")[0]);
+
     showElements(document.getElementsByTagName("label")[0]);
     showElements(selectCategoriesMenu);
     showElements(compareButton);
@@ -173,7 +177,6 @@ shuffleButton.addEventListener("click", () => {
 selectCategoriesMenu.addEventListener("change", (event) => {
 
     category = event.target.value
-    console.log(category)
 
 })
 
@@ -185,10 +188,8 @@ compareButton.addEventListener("click", () => {
         playerTwoTopCard.getElementsByTagName("img")[0].style.margin = "auto";
         playerTwoTopCard.getElementsByTagName("h3")[0].style.display = "inline-block";
         playerTwoTopCard.getElementsByTagName("table")[0].style.display = "inline-block";
-
         playerTwoTopCard.style.color = "#00165e";
         playerTwoTopCard.style.backgroundColor = "#9c1203";
-    
         playerTwoTopCard.getElementsByTagName("div")[0].remove();
 
     } else {
@@ -197,10 +198,8 @@ compareButton.addEventListener("click", () => {
         playerOneTopCard.getElementsByTagName("img")[0].style.margin = "auto";
         playerOneTopCard.getElementsByTagName("h3")[0].style.display = "inline-block";
         playerOneTopCard.getElementsByTagName("table")[0].style.display = "inline-block";
-
         playerOneTopCard.style.color = "#00165e";
         playerOneTopCard.style.backgroundColor = "#9c1203";
-
         playerOneTopCard.getElementsByTagName("div")[0].remove();
     }
 
@@ -224,7 +223,9 @@ compareButton.addEventListener("click", () => {
             showElements(document.getElementsByClassName("round limbo")[0])
         }
 
-        showPlayer(roundWinner, document.getElementsByClassName("round winner")[0].getElementsByTagName("span")[0] )
+        showPlayer(roundWinner, document.getElementsByClassName("round winner")[0].getElementsByClassName("player")[0] );
+        document.getElementsByClassName("round winner")[0].getElementsByClassName("category-name")[0].innerHTML = category; 
+     
 
     });
 
@@ -280,14 +281,14 @@ compareButton.addEventListener("click", () => {
         hideElements(document.getElementsByClassName("round winner")[0]);
         hideElements(document.getElementsByClassName("round limbo")[0]);
 
-        if (playerOne.length == witchesWizards.length || playerTwo.length == witchesWizards.length){
+        if (playerOne.length == graduates.length || playerTwo.length == graduates.length){
 
             showPlayer(roundWinner, document.getElementsByClassName("game winner")[0].getElementsByTagName("span")[0] )
             showElements(document.getElementsByClassName("game winner")[0]);
             showElements(document.getElementsByClassName("new game")[0]);
             showElements(shuffleButton);
 
-            if (playerOne.length == witchesWizards.length){
+            if (playerOne.length == graduates.length){
 
                 hideElements(document.getElementsByClassName("player-two")[0]);
                 populateCards(playerOneTopCard, playerOne);
@@ -331,6 +332,8 @@ compareButton.addEventListener("click", () => {
             
             populateCards(playerTwoTopCard, playerTwo)
             document.getElementsByClassName("player-two")[0].getElementsByClassName("number-of-cards")[0].innerHTML = playerTwo.length;
+
+            document.getElementsByClassName("set-aside")[0].getElementsByTagName("span")[0].innerHTML = limbo.length;
 
             showPlayer(playerTurn, document.getElementsByClassName("turn")[0].getElementsByTagName("span")[0]);
             showElements(document.getElementsByClassName("turn")[0]);
