0fb4b689becfdd7f022b956f57e693f3539f00f6
Chenerywoman <rachelmchenery@hotmail.com>
2020-11-23 17:46:16 +0000

    removing functions which manipulate the DOM

--- backend_versions/helpers.js
+++ backend_versions/helpers.js
@@ -1,21 +1,3 @@
-const hideElements = (element) => {
-    element.style.display = "none";
-}
-
-const showElements = (element) => {
-    element.style.display = "inline-block";
-}
-
-const populateCards = (element, array) => {
-    element.getElementsByTagName("img")[0].src = array[0].image;
-    element.getElementsByTagName("img")[0].alt = array[0].alt;
-    element.getElementsByClassName("witch-wizard")[0].getElementsByTagName("span")[0].innerText = array[0].name;
-    element.getElementsByClassName("matriculation")[0].innerText = array[0].matriculation;
-    element.getElementsByClassName("wand")[0].innerText = array[0].wand;
-    element.getElementsByClassName("potions")[0].innerText = array[0].potions;
-    element.getElementsByClassName("spells")[0].innerText = array[0].spells;
-    element.getElementsByClassName("courage")[0].innerText = array[0].courage;
-}
 
 const addOrUpdate = (name, array) => {
 
@@ -52,9 +34,6 @@ const getNames = (arrayOfObjects) => {
 }
 
 module.exports = {
-    populateCards,
-    hideElements, 
-    showElements, 
     addOrUpdate,
     getHighest, 
     getNames
